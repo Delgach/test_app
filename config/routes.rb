658fb92a5e0e9873c6e4be02034841ab061fb5c8
Delgach <vladimir@MacBook-Pro-Delgadilo.local>
2017-09-17 03:00:01 +0300

    another commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'project/index'
 
-  # post 'todo_create' => 'todo#create' , as: 'create_todo'
+  #post 'todo_create' => 'todo#create' , as: 'create_tad'
   get 'todo_create' => 'todo#create' , as: 'create_todo'
  
-  root :to =>'project#index'# , as: 'root'
+  root :to =>'project#index' , as: 'root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
